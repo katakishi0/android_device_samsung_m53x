429152cf0e03421cb86786b9efaba281b0fe6ab8
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-25 19:47:22 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,23 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright 2019 The Android Open Source Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_m53x.mk
+    $(LOCAL_DIR)/twrp_m53x.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_m53x-user \
-    omni_m53x-userdebug \
-    omni_m53x-eng
+    twrp_m53x-user \
+    twrp_m53x-userdebug \
+    twrp_m53x-eng
