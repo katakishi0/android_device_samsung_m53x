7750dfe50fcb8d7863568bab4266e42a2e0fcc0a
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-26 06:47:41 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,105 +1,65 @@
-#
-# Copyright (C) 2022 The OrangeFox Recovery Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
+#
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2021-2022 The OrangeFox Recovery Project
+#
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
+#
+# 	Please maintain this if you use this script or any part of it
+#
 FDEVICE="m53x"
 
 fox_get_target_device() {
-local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
-	if [ -n "$chkdev" ]; then
-		FOX_BUILD_DEVICE="$FDEVICE"
-	else
-		chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
-		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
-	fi
+local chkdev=$(echo "$BASH_SOURCE" | grep -w \"$FDEVICE\")
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w \"$FDEVICE\")
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
 }
 
 if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
-	fox_get_target_device
+   echo "** WARNING **: Always set FOX_BUILD_DEVICE to the device codename before starting to build for any device!"
+   fox_get_target_device
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
-	export ALLOW_MISSING_DEPENDENCIES=true
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export TARGET_DEVICE_ALT="m53x"
-	export FOX_TARGET_DEVICES="m53x"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1588606644"
-
-	# Screen Settings
-	export OF_SCREEN_H="2400"
-	export OF_STATUS_H="105"
-	export OF_STATUS_INDENT_LEFT="48"
-	export OF_STATUS_INDENT_RIGHT="48"
-	export OF_ALLOW_DISABLE_NAVBAR="0"
-
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v25.2.zip
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	
-	# OTA
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
-    
-	# R11.1 Setings
-	export FOX_BUILD_TYPE="Beta"
-	export OF_MAINTAINER="SquirrelPython"
-	export FOX_VERSION="R11.1_3"
-    
-	export FOX_ENABLE_APP_MANAGER=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export FOX_R11=1
-	export OF_FLASHLIGHT_ENABLE=1
-	export FOX_DELETE_AROMAFM=1
-	export OF_PATCH_AVB20=1
-	export OF_FBE_METADATA_MOUNT_IGNORE="1"
-	export OF_HIDE_NOTCH="1"
-	export FOX_REPLACE_BUSYBOX_PS="0"
-	export FOX_SKIP_ZIP_BINARY="1"
-	export OF_ADVANCED_SECURITY=1
-
-	# Magisk
-	if [ -n "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
-		if [ ! -e "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
-			echo "Downloading the Latest Release of Magisk..."
-			LATEST_MAGISK_URL="$(curl -sL https://api.github.com/repos/topjohnwu/Magisk/releases/latest | grep browser_download_url | grep Magisk- | cut -d : -f 2,3 | sed 's/"//g')"
-			aria2c ${LATEST_MAGISK_URL} -o ${FOX_USE_SPECIFIC_MAGISK_ZIP} || wget ${LATEST_MAGISK_URL} -O ${FOX_USE_SPECIFIC_MAGISK_ZIP}
-			[ $? = "0" ] && echo "Magisk Downloaded Successfully"
-			echo "Done!"
-		fi
-	fi
-
-	# let's see what are our Build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
+        export ALLOW_MISSING_DEPENDENCIES="true"
+        export OF_DISABLE_MIUI_SPECIFIC_FEATURES="1"
+        export FOX_VANILLA_BUILD="1"
+        export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+        export OF_SCREEN_H="2400"
+        export OF_STATUS_H="140"
+        export OF_STATUS_INDENT_LEFT="48"
+        export OF_STATUS_INDENT_RIGHT="48"
+        export OF_USE_MAGISKBOOT="1"
+        export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+        export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+        export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+        export LC_ALL="C"
+        export FOX_USE_NANO_EDITOR="1"
+        export FOX_DELETE_AROMAFM="1"
+        export FOX_USE_TAR_BINARY="1"
+        export FOX_USE_SED_BINARY="1"
+        export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+        export FOX_USE_XZ_UTILS="1"
+        export FOX_VARIANT="A14"
+        export OF_MAINTAINER="Katakishi0"
+        export FOX_BUILD_TYPE="Stable"
+        export FOX_VERSION="R11.1_4"
+        export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.qcom.rc for more information
+        export FOX_DELETE_MAGISK_ADDON="1"
+        export OF_USE_GREEN_LED="0"
 fi
